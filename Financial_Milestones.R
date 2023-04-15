e56b60e8dc705416ff7706b3e4002f3200bf8ba9
saimachi <saikmachi@gmail.com>
2023-04-15 16:26:46 -0700

    Added map & annotations

---
+++ Financial_Milestones.R
@@ -0,0 +1,48 @@
+library(ggmap)
+library(ggplot2)
+library(ggrepel)
+
+milestones.coords = data.frame(
+  lat = c(28.53551, 21.1458, 30.73331, 12.06239, 10.16315),
+  lon = c(77.39102, 79.08815, 76.77941, 78.58567, 76.64127),
+  city = c(
+    "Noida, Uttar Pradesh",
+    "Nagpur, Maharashtra",
+    "Chandigarh",
+    "Katpadi, Tamil Nadu",
+    "Kerala"
+  ),
+  remark = c(
+    "Fastest adoption of Paytm by small vendors",
+    "Fastest adoption of Paytm by small vendors",
+    "ATMs per capita frontrunner",
+    "Fastest growth of digital payments",
+    "Winner of three Digital India awards"
+  )
+)
+
+# Organize cities in the key by latitude (decreasing)
+milestones.coords$city = factor(milestones.coords$city, levels = milestones.coords[order(-milestones.coords$lat), "city"])
+
+# Call register_google() with Maps API key
+map = get_map(location = "India", zoom = 5)
+map = ggmap(map)
+map = map +
+  geom_point(data = milestones.coords,
+             aes(x = lon, y = lat, color = city),
+             size = 5) +
+  geom_label_repel(
+    data = milestones.coords,
+    aes(x = lon, y = lat, label = remark),
+    box.padding = 1,
+    segment.size = 1
+  ) +
+  labs(
+    x = "Longitude",
+    y = "Latitude",
+    title = "(Fig. 6) State of Digital Payments in India",
+    subtitle = "A variety of Indian states have seen rapid growth in digital financial infrastructure.",
+    color = "Location"
+  ) +
+  theme(legend.position = "bottom")
+ggsave("plots/milestones.png", map)
